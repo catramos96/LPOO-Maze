aee04c142da69a14e054de0ba26f483f3859d245
catramos96 <catramos96@gmail.com>
2016-03-05 22:56:27 +0000

    Added new test class TestRandomDragonBehavior
    ...

---
+++ src/test/TestRandomDragonBehavior.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import maze.logi.Board;
+import maze.logi.Point;
+
+public class TestRandomDragonBehavior {
+
+	char[][] m1 = { { 'X', 'X', 'X', 'X', 'X' }, { 'X', ' ', ' ', 'H', 'S' }, { 'X', ' ', 'X', ' ', 'X' },
+			{ 'X', 'E', ' ', 'D', 'X' }, { 'X', 'X', 'X', 'X', 'X' } };
+
+	// x - the columns
+	// y - the lines
+	// Point(x,y);
+
+	/*
+	 * @Test public void testMoveHeroToFreeCell() { Board b = new Board(m1);
+	 * assertEquals(new Point(3, 1), b.getHero().getPosition());
+	 * b.moveHero('a'); // move to left assertEquals(new Point(2, 1),
+	 * b.getHero().getPosition()); }
+	 */
+
+}
